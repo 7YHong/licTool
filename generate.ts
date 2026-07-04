6d35aa3b1f3b1a2f92bdd4faa5c622390d8db56b
7YHong <qiyanghong@qq.com>
2026-07-05 00:32:36 +0800

    修复中文错误

--- generate.ts
+++ generate.ts
@@ -1,11 +1,11 @@
 import forge from "node-forge";
 
 const ED25519 = forge.pki.ed25519;
-const decode64 = forge.util.decode64;
+const { decode64, createBuffer } = forge.util;
 
 export const generate = (payloadStr: string, privateKey: string) => {
   // Base64 编码 payload (使用标准 base64)
-  const base64Payload = forge.util.encode64(payloadStr);
+  const base64Payload = forge.util.encode64(createBuffer(payloadStr, "utf8").getBytes());
 
   // 生成 SHA-256 摘要
   const md = forge.md.sha256.create();
--- verify.ts
+++ verify.ts
@@ -1,14 +1,14 @@
 import forge from "node-forge";
 
 const ED25519 = forge.pki.ed25519;
-const decode64 = forge.util.decode64;
+const { decode64, createBuffer } = forge.util;
 
 export const verify = (licStr: string, publicKey: string) => {
   const [payload, signature] = licStr.split(".");
-  if(!payload || !signature) {
+  if (!payload || !signature) {
     throw new Error("授权码格式错误");
   }
-  if(decode64(signature).length !== 64) {
+  if (decode64(signature).length !== 64) {
     throw new Error("授权码格式错误: 签名长度错误");
   }
   // 生成 SHA-256 摘要
@@ -23,5 +23,5 @@ export const verify = (licStr: string, publicKey: string) => {
     throw new Error("签名验证失败，数据可能被篡改");
   }
 
-  return forge.util.decode64(payload ?? "");
+  return createBuffer(decode64(payload ?? "")).toString();
 };
